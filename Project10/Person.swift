6c0365db12437e0b91db220bd40c89cac1b69b18
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-04-18 14:44:34 +0300

    UserDefaults with NSCoding

--- Project10/Person.swift
+++ Project10/Person.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
     var name: String
     var image: String
     
     init(name: String, image: String) {
         self.image = image
         self.name = name
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
     }
 }
