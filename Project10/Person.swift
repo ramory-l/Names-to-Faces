f8c81c730ff41ecd0d386c06eccd124633691fc2
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-04-18 14:54:35 +0300

    userDefaults with Codable

--- Project10/Person.swift
+++ Project10/Person.swift
@@ -8,22 +8,12 @@
 
 import UIKit
 
-class Person: NSObject, NSCoding {
+class Person: NSObject, Codable {
     var name: String
     var image: String
     
     init(name: String, image: String) {
         self.image = image
         self.name = name
-    }
-    
-    required init?(coder: NSCoder) {
-        name = coder.decodeObject(forKey: "name") as? String ?? ""
-        image = coder.decodeObject(forKey: "image") as? String ?? ""
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(name, forKey: "name")
-        coder.encode(image, forKey: "image")
     }
 }
